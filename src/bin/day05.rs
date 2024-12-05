7337d17ef55e6b140f306afe371e5c5a9d4f15e3
Jon Chambers <jon.chambers@gmail.com>
2024-12-05 00:58:28 -0500

    Add a solution for day 5, part 2

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -21,6 +21,11 @@ fn main() -> Result<(), Box<dyn Error>> {
             manual_updater.middle_page_sum_from_correct_updates()
         );
 
+        println!(
+            "Sum of middle pages from repaired, incorrectly-ordered updates: {}",
+            manual_updater.middle_page_sum_from_repaired_incorrect_updates()
+        );
+
         Ok(())
     } else {
         Err("Usage: day05 INPUT_FILE_PATH".into())
@@ -41,6 +46,15 @@ impl ManualUpdater {
             .sum()
     }
 
+    pub fn middle_page_sum_from_repaired_incorrect_updates(&self) -> u32 {
+        self.updates
+            .iter()
+            .filter(|update| !self.has_correct_order(update))
+            .map(|update| self.repair_update(update))
+            .map(|update| update[update.len() / 2])
+            .sum()
+    }
+
     fn has_correct_order(&self, update: &[u32]) -> bool {
         for i in 1..update.len() {
             if let Some(constraints) = self.constraints.get(&update[i]) {
@@ -51,6 +65,58 @@ impl ManualUpdater {
         }
 
         true
+    }
+
+    fn repair_update(&self, update: &[u32]) -> Vec<u32> {
+        let mut unresolved_constraints = HashMap::new();
+
+        update.iter().for_each(|page| {
+            if let Some(constraints) = self.constraints.get(page) {
+                unresolved_constraints.insert(
+                    *page,
+                    constraints
+                        .iter()
+                        .filter(|page| update.contains(page))
+                        .to_owned()
+                        .collect(),
+                );
+            } else {
+                unresolved_constraints.insert(*page, Vec::new());
+            }
+        });
+
+        let mut repaired_update = Vec::with_capacity(update.len());
+
+        while !unresolved_constraints.is_empty() {
+            // One page in the update should have no constraints, and we can push it into the
+            // repaired order
+            if let Some(page) = unresolved_constraints
+                .iter()
+                .filter(|(_, constraints)| constraints.is_empty())
+                .map(|(page, _)| *page)
+                .next()
+            {
+                repaired_update.push(page);
+                unresolved_constraints.remove(&page);
+
+                unresolved_constraints
+                    .iter_mut()
+                    .for_each(|(_, constraints)| {
+                        if let Some(i) = constraints
+                            .iter()
+                            .position(|&&constraint| constraint == page)
+                        {
+                            constraints.remove(i);
+                        }
+                    });
+            } else {
+                panic!("Unresolvable constraints");
+            }
+        }
+
+        repaired_update.reverse();
+
+        repaired_update
     }
 }
 
@@ -141,5 +207,32 @@ mod test {
     fn test_middle_page_sum_from_correct_updates() {
         let manual_updater = ManualUpdater::from_str(TEST_RULES_AND_UPDATES).unwrap();
         assert_eq!(143, manual_updater.middle_page_sum_from_correct_updates());
+    }
+
+    #[test]
+    fn test_repair_update() {
+        let manual_updater = ManualUpdater::from_str(TEST_RULES_AND_UPDATES).unwrap();
+
+        assert_eq!(
+            vec![97, 75, 47, 61, 53],
+            manual_updater.repair_update(&[75, 97, 47, 61, 53])
+        );
+        assert_eq!(
+            vec![61, 29, 13],
+            manual_updater.repair_update(&[61, 13, 29])
+        );
+        assert_eq!(
+            vec![97, 75, 47, 29, 13],
+            manual_updater.repair_update(&[97, 13, 75, 29, 47])
+        );
+    }
+
+    #[test]
+    fn test_middle_page_sum_from_repaired_incorrect_updates() {
+        let manual_updater = ManualUpdater::from_str(TEST_RULES_AND_UPDATES).unwrap();
+        assert_eq!(
+            123,
+            manual_updater.middle_page_sum_from_repaired_incorrect_updates()
+        );
     }
 }
