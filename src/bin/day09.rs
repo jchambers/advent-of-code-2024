d6fff4a68c42b0d0fdc5e6ff052039e7c8dff81c
Jon Chambers <jon.chambers@gmail.com>
2024-12-09 16:22:56 -0500

    Add a solution for day 9, part 2

--- src/bin/day09.rs
+++ src/bin/day09.rs
@@ -9,6 +9,11 @@ fn main() -> Result<(), Box<dyn Error>> {
         let disk_map = DiskMap::from_str(fs::read_to_string(path)?.as_str())?;
 
         println!("Defrag checksum: {}", disk_map.defrag_checksum());
+        
+        println!(
+            "Whole-file defrag checksum: {}",
+            disk_map.whole_file_defrag_checksum()
+        );
 
         Ok(())
     } else {
@@ -26,10 +31,10 @@ impl DiskMap {
         let mut blocks_written = 0;
         let mut checksum = 0;
 
-        for digit_index in 0..self.layout.len() {
-            let is_file_entry = digit_index % 2 == 0;
+        for entry in 0..self.layout.len() {
+            let is_file_entry = entry % 2 == 0;
 
-            for _ in 0..self.layout[digit_index] {
+            for _ in 0..self.layout[entry] {
                 if let Some(file_id) = if is_file_entry {
                     file_blocks.next()
                 } else {
@@ -45,6 +50,92 @@ impl DiskMap {
         }
 
         checksum
+    }
+
+    fn whole_file_defrag_checksum(&self) -> u64 {
+        let mut disk = {
+            let total_blocks = self
+                .layout
+                .iter()
+                .map(|&block_len| block_len as usize)
+                .sum();
+
+            let mut disk = vec![None; total_blocks];
+            let mut write_index = 0usize;
+
+            for entry in 0..self.layout.len() {
+                let is_file_entry = entry % 2 == 0;
+
+                if is_file_entry {
+                    let file_id = entry / 2;
+                    disk[write_index..write_index + self.layout[entry] as usize]
+                        .fill(Some(file_id));
+                }
+
+                write_index += self.layout[entry] as usize;
+            }
+
+            disk
+        };
+
+        let mut undefragmented: &mut [Option<usize>] = &mut disk;
+
+        while !undefragmented.is_empty() {
+            if let Some(first_empty_block) = undefragmented.iter().position(|&x| x.is_none()) {
+                undefragmented = &mut undefragmented[first_empty_block..];
+
+                if let Some((file_id, file_start, file_len)) = Self::last_file(undefragmented) {
+                    if let Some(empty_span_start) = Self::next_empty_span(undefragmented, file_len)
+                    {
+                        undefragmented[empty_span_start..empty_span_start + file_len]
+                            .fill(Some(file_id));
+                        undefragmented[file_start..file_start + file_len].fill(None);
+                    }
+
+                    // Whether or not we could find space for this file, we've taken our one shot
+                    // at it and don't need to inspect it again
+                    undefragmented = &mut undefragmented[..file_start];
+                } else {
+                    // The rest of the disk is empty; we're done!
+                    break;
+                }
+            } else {
+                // No more empty space; we're done!
+                break;
+            }
+        }
+
+        disk.iter()
+            .map(|block| block.unwrap_or(0))
+            .enumerate()
+            .map(|(i, x)| (i * x) as u64)
+            .sum()
+    }
+
+    fn next_empty_span(blocks: &[Option<usize>], len: usize) -> Option<usize> {
+        let target = vec![None; len];
+
+        blocks.windows(len).position(|window| window == target)
+    }
+
+    fn last_file(blocks: &[Option<usize>]) -> Option<(usize, usize, usize)> {
+        blocks
+            .iter()
+            .enumerate()
+            .rev()
+            .find_map(|(i, block)| block.map(|b| (i, b)))
+            .map(|(end, file_id)| {
+                blocks
+                    .iter()
+                    .enumerate()
+                    .rev()
+                    .skip_while(|(_, block)| block.is_none())
+                    .take_while(|(_, block)| block == &&Some(file_id))
+                    .map(|(i, _)| i)
+                    .last()
+                    .map(|start| (file_id, start, end - start + 1))
+                    .unwrap()
+            })
     }
 
     fn total_file_blocks(layout: &[u8]) -> usize {
@@ -165,5 +256,17 @@ mod test {
     fn test_defrag_checksum() {
         let disk_map = DiskMap::from_str("2333133121414131402").unwrap();
         assert_eq!(1928, disk_map.defrag_checksum());
+    }
+
+    #[test]
+    fn test_whole_file_defrag_checksum() {
+        let disk_map = DiskMap::from_str("2333133121414131402").unwrap();
+        assert_eq!(2858, disk_map.whole_file_defrag_checksum());
+    }
+
+    #[test]
+    fn test_last_file() {
+        let blocks = [None, None, Some(0), None, Some(7), Some(7), Some(7)];
+        assert_eq!(Some((7, 4, 3)), DiskMap::last_file(&blocks));
     }
 }
