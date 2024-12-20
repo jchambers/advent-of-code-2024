4147fa06fe34a23764969c144ddeb5ed7fcd5ef2
Jon Chambers <jon.chambers@gmail.com>
2024-12-20 17:54:46 -0500

    Generalize day 20

--- src/bin/day20.rs
+++ src/bin/day20.rs
@@ -7,7 +7,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     if let Some(path) = args.get(1) {
         let racetrack = RaceTrack::from_str(fs::read_to_string(path)?.as_str())?;
-        let cheats = racetrack.cheats();
+        let cheats = racetrack.cheats(2);
 
         println!(
             "Cheats saving at least 100 picoseconds: {}",
@@ -29,70 +29,88 @@ struct RaceTrack {
 }
 
 impl RaceTrack {
-    pub fn cheats(&self) -> Vec<u32> {
-        let main_path = self.distances_on_main_path();
+    pub fn cheats(&self, radius: usize) -> Vec<u32> {
+        let times_along_path = self.times_along_path();
         let mut cheats = Vec::new();
 
-        // Look for horizontal cheats
-        let height = self.tiles.len() / self.width;
+        times_along_path
+            .iter()
+            .enumerate()
+            .filter_map(|(i, elapsed_time)| elapsed_time.map(|t| (i, t)))
+            .for_each(|(i, elapsed_time)| {
+                self.neighbors(i, radius)
+                    .iter()
+                    .filter_map(|&neighbor| times_along_path[neighbor].map(|t| (neighbor, t)))
+                    .for_each(|(neighbor, neighbor_time)| {
+                        let distance = self.distance(i, neighbor);
 
-        for y in 1..height - 1 {
-            for x in 1..self.width - 2 {
-                let start = (y * self.width) + x;
-
-                if let [Some(a), None, Some(b)] = main_path[start..start + 3] {
-                    cheats.push(a.abs_diff(b) - 2);
-                }
-            }
-        }
-
-        // …and look for vertical cheats
-
-        for x in 1..self.width - 1 {
-            for y in 1..height - 2 {
-                let start = (y * self.width) + x;
-
-                if let [Some(a), None, Some(b)] = [
-                    main_path[start],
-                    main_path[start + self.width],
-                    main_path[start + (self.width * 2)],
-                ] {
-                    cheats.push(a.abs_diff(b) - 2);
-                }
-            }
-        }
+                        if neighbor_time > elapsed_time + distance {
+                            // This is actually a shortcut
+                            cheats.push(neighbor_time - elapsed_time - distance);
+                        }
+                    });
+            });
 
         cheats
     }
 
-    fn distances_on_main_path(&self) -> Vec<Option<u32>> {
+    fn neighbors(&self, index: usize, radius: usize) -> Vec<usize> {
+        let height = self.tiles.len() / self.width;
+
+        let (x, y) = (index % self.width, index / self.width);
+
+        let x_min = x.saturating_sub(radius);
+        let x_max = (x + radius).min(self.width - 1);
+        let y_min = y.saturating_sub(radius);
+        let y_max = (y + radius).min(height - 1);
+
+        let mut neighbors = Vec::new();
+
+        for x_neighbor in x_min..=x_max {
+            for y_neighbor in y_min..=y_max {
+                if x.abs_diff(x_neighbor) + y.abs_diff(y_neighbor) <= radius
+                    && (x != x_neighbor || y != y_neighbor)
+                {
+                    neighbors.push((self.width * y_neighbor) + x_neighbor);
+                }
+            }
+        }
+
+        neighbors
+    }
+
+    fn distance(&self, a: usize, b: usize) -> u32 {
+        let (x_a, y_a) = (a % self.width, a / self.width);
+        let (x_b, y_b) = (b % self.width, b / self.width);
+
+        (x_a.abs_diff(x_b) + y_a.abs_diff(y_b)) as u32
+    }
+
+    fn times_along_path(&self) -> Vec<Option<u32>> {
         let mut distances = vec![None; self.tiles.len()];
         distances[self.start] = Some(0);
 
-        let mut position = self.start;
+        let mut index = self.start;
         let mut distance = 0;
 
         // The problem statement asserts that there's exactly one path through the maze, and we
         // additionally assert that the whole racetrack has a solid border of walls.
         loop {
-            if position == self.end {
+            if index == self.end {
                 break;
             }
 
             distance += 1;
 
-            let next = *[
-                position + 1,
-                position - 1,
-                position + self.width,
-                position - self.width,
-            ]
-            .iter()
-            .find(|&&neighbor| self.tiles[neighbor] == Tile::Track && distances[neighbor].is_none())
-            .unwrap();
+            let next = *[index + 1, index - 1, index + self.width, index - self.width]
+                .iter()
+                .find(|&&neighbor| {
+                    self.tiles[neighbor] == Tile::Track && distances[neighbor].is_none()
+                })
+                .unwrap();
 
             distances[next] = Some(distance);
-            position = next;
+            index = next;
         }
 
         distances
@@ -139,8 +157,8 @@ impl FromStr for RaceTrack {
             tiles,
             width,
 
-            start: start.ok_or("Start position not found")?,
-            end: end.ok_or("End position not found")?,
+            start: start.ok_or("Start index not found")?,
+            end: end.ok_or("End index not found")?,
         })
     }
 }
@@ -177,7 +195,7 @@ mod test {
     #[test]
     fn test() {
         let racetrack = RaceTrack::from_str(TEST_RACETRACK).unwrap();
-        let cheats = racetrack.cheats();
+        let cheats = racetrack.cheats(2);
 
         for (n, savings) in [
             (14, 2),
