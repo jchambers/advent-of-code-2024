00bbe80375f4eb31565cf514ffa690d48b85e274
Jon Chambers <jon.chambers@gmail.com>
2024-12-22 12:40:13 -0500

    Add a solution for day 22, part 2

--- src/bin/day22.rs
+++ src/bin/day22.rs
@@ -7,15 +7,22 @@ fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
 
     if let Some(path) = args.get(1) {
-        let seeds: Vec<u64> = BufReader::new(File::open(path)?)
-            .lines()
-            .map_while(Result::ok)
-            .map(|line| line.parse())
-            .collect::<Result<Vec<_>, _>>()?;
+        let monkey_market = MonkeyMarket {
+            secret_numbers: BufReader::new(File::open(path)?)
+                .lines()
+                .map_while(Result::ok)
+                .map(|line| line.parse())
+                .collect::<Result<Vec<_>, _>>()?,
+        };
 
         println!(
             "Secret number sum after 2000 iterations: {}",
-            secret_number_sum(&seeds, 2000)
+            monkey_market.secret_number_sum(2000)
+        );
+
+        println!(
+            "Max bananas after 2000 iterations: {}",
+            monkey_market.max_bananas(2000)
         );
 
         Ok(())
@@ -24,11 +31,67 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 }
 
-fn secret_number_sum(seeds: &[u64], iterations: usize) -> u64 {
-    seeds
-        .iter()
-        .map(|&seed| SecretNumbers::new(seed).take(iterations).last().unwrap())
-        .sum()
+struct MonkeyMarket {
+    secret_numbers: Vec<u64>,
+}
+
+impl MonkeyMarket {
+    pub fn secret_number_sum(&self, iterations: usize) -> u64 {
+        self.secret_numbers
+            .iter()
+            .map(|&seed| SecretNumbers::new(seed).take(iterations).last().unwrap())
+            .sum()
+    }
+
+    pub fn max_bananas(&self, iterations: usize) -> u32 {
+        const SEQUENCE_LENGTH: usize = 4;
+        const DISTINCT_SEQUENCES: usize = 19usize.pow(SEQUENCE_LENGTH as u32);
+
+        let mut max_bananas_by_sequence = vec![0; DISTINCT_SEQUENCES];
+
+        self.secret_numbers
+            .iter()
+            .map(|&seed| {
+                let mut prices = Vec::with_capacity(iterations + 1);
+                prices.push((seed % 10) as i8);
+
+                prices.extend(
+                    SecretNumbers::new(seed)
+                        .take(iterations)
+                        .map(|n| (n % 10) as i8),
+                );
+
+                prices
+            })
+            .for_each(|prices| {
+                let deltas: Vec<i8> = prices
+                    .windows(2)
+                    .map(|window| window[1] - window[0])
+                    .collect();
+
+                let mut encountered_sequences = vec![false; DISTINCT_SEQUENCES];
+
+                deltas
+                    .windows(SEQUENCE_LENGTH)
+                    .zip(prices.iter().skip(SEQUENCE_LENGTH))
+                    .for_each(|(sequence, &price)| {
+                        let packed_sequence = Self::pack_sequence(sequence) as usize;
+
+                        if !encountered_sequences[packed_sequence] {
+                            encountered_sequences[packed_sequence] = true;
+                            max_bananas_by_sequence[packed_sequence] += price as u32;
+                        }
+                    });
+            });
+
+        *max_bananas_by_sequence.iter().max().unwrap()
+    }
+
+    fn pack_sequence(sequence: &[i8]) -> u32 {
+        sequence
+            .iter()
+            .fold(0u32, |acc, &i| acc * 19 + ((i + 9) as u32))
+    }
 }
 
 struct SecretNumbers {
@@ -73,6 +136,17 @@ mod test {
 
     #[test]
     fn test_secret_number_sum() {
-        assert_eq!(37327623, secret_number_sum(&[1, 10, 100, 2024], 2000));
+        let monkey_market = MonkeyMarket {
+            secret_numbers: vec![1, 10, 100, 2024],
+        };
+        assert_eq!(37327623, monkey_market.secret_number_sum(2000));
+    }
+
+    #[test]
+    fn test_max_bananas() {
+        let monkey_market = MonkeyMarket {
+            secret_numbers: vec![1, 2, 3, 2024],
+        };
+        assert_eq!(23, monkey_market.max_bananas(2000));
     }
 }
